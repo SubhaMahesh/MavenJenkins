d454a30d8c2d16a1aa76f723329949f5047eb980
Subhasri <subhacertification@outlook.com>
2025-01-20 19:34:12 +0530

    third

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class UITest 
@@ -17,7 +17,7 @@ public class UITest
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
-	//	Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
 		driver.quit();
 	}
 	
